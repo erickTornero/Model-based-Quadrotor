401dc3e5f6f02078fcda9768a7a55bb5b041f388
erickTornero <erickdeivy01@gmail.com>
2019-09-25 19:02:18 -0500

    fixing hyperparameters & plot scattering positions

--- mbrl/run_experiments.py
+++ mbrl/run_experiments.py
@@ -15,12 +15,14 @@ import joblib
 from utils.plots import *
 from IPython.core.debugger import set_trace
 
+from tensorboardX import SummaryWriter
+
 """*****************************************
     Hyper-Parameters Settings
 ********************************************
 """
 """ MPC Controller - Random Shooting """
-horizon     =   10
+horizon     =   15
 candidates  =   1000
 discount    =   0.99
 
@@ -35,8 +37,9 @@ validation_percent      =   0.2
 learning_rate           =   1e-4
 
 """ General parameters """
-id_executor             =   'sample2'
-n_iterations            =   24
+id_executor             =   'sample3'
+n_iterations            =   36
+
 save_path               =   os.path.join('./data/', id_executor)
 
 """************************
@@ -70,6 +73,7 @@ runner = Runner(vecenv, env_, dyn, rs, max_path_length, total_tsteps_per_run)
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
+writer = SummaryWriter()
 mean_reward_maximum =   0.0
 for n_it in range(1, n_iterations+1):
     print('============================================')
@@ -82,24 +86,29 @@ for n_it in range(1, n_iterations+1):
     delta_obs       =   paths['delta_obs']
     total_rewards   =   paths['rewards']
     data_x          =   np.concatenate((observations, actions), axis=1)
+    """ Save model with high rewards """
+    mean_reward     =   np.mean(total_rewards)
+    writer.add_scalar('data/reward', mean_reward, n_it)
+    if mean_reward_maximum < mean_reward:
+        print('Saving new highest reward') 
+        mean_reward_maximum = mean_reward
+        torch.save(dyn.state_dict(), os.path.join(save_path, 'params_high.pkl'))
+
     tr_loss, vl_loss = trainer.fit(data_x, delta_obs)
     print('-------------Info {}-------------'.format(n_it))
     rolls_info      =   vecenv.get_reset_nrollouts()
     print('Rolls per env> {}, total rollouts {}'.format(rolls_info, sum(rolls_info)))
     print('total time steps: \t{}'.format(actions.shape[0]))
-    print('Reward mean: \t\t{}'.format(np.mean(total_rewards)))
+    print('Reward mean: \t\t{}'.format(mean_reward))
     print('Reward  std: \t\t{}'.format(np.std(total_rewards)))
     print('Reward  min: \t\t{}'.format(np.min(total_rewards)))
     print('Reward  max: \t\t{}'.format(np.max(total_rewards)))
     
     print('Saving model ...')
-    if mean_reward_maximum < np.mean(total_rewards): 
-        mean_reward_maximum = np.mean(total_rewards)
-        torch.save(dyn.state_dict(), os.path.join(save_path, 'params_high.pkl'))
-    else:
-        torch.save(dyn.state_dict(), os.path.join(save_path, 'params.pkl'))
+    torch.save(dyn.state_dict(), os.path.join(save_path, 'params.pkl'))
 
     joblib.dump(observations, os.path.join(save_path, 'observations_it_' + str(n_it)+'.pkl'))
+    joblib.dump(total_rewards, os.path.join(save_path, 'rewards_it_'+str(n_it)+'.pkl'))
     plot_loss_per_iteration(tr_loss, vl_loss, os.path.join(save_path, 'loss_it_'+str(n_it)+'.png'))
 
 #paths = runner.run(random=False)
--- utils/plots.py
+++ utils/plots.py
@@ -9,3 +9,36 @@ def plot_loss_per_iteration(tr_loss, val_loss, save_path):
     plt.legend(['Training loss', 'Validation Loss'])
 
     plt.savefig(save_path)
+
+
+def plot_scatter_positions(data, index_start_pos, save_path=None):
+    """ Plot distributions of positions in X-Y, X-Z, Y-Z"""
+    x_data   =   data[:, index_start_pos]
+    y_data   =   data[:, index_start_pos + 1]
+    z_data   =   data[:, index_start_pos + 2]
+
+    """ Initialize subplot 1 x 3 """
+    plt.figure(figsize=(12, 4))
+    
+
+    """ Distribution X-Y"""
+    plt.subplot(1, 3, 1)
+    plt.scatter(x_data, y_data, alpha=0.2, marker='o', s=5, color='blue')
+    plt.xlim(-3.2, 3.2)
+    plt.ylim(-3.2, 3.2)
+
+    """ Distribution of Position in X-Z"""
+    plt.subplot(1, 3, 2)
+    plt.scatter(x_data, z_data, alpha=0.2, marker='o', s=5, color='red')
+    plt.xlim(-3.2, 3.2)
+    plt.ylim(-3.2, 3.2)
+
+    """ Distribution of Position in Y-Z"""
+    plt.subplot(1, 3, 3)
+    plt.scatter(y_data, z_data, alpha=0.2, marker='o', s=5, color='gray')
+    plt.xlim(-3.2, 3.2)
+    plt.ylim(-3.2, 3.2)
+
+    """ Final commands """
+    plt.tight_layout()
+    plt.show() if save_path is None else plt.savefig(save_path)
