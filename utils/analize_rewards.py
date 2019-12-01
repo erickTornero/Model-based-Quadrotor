984ba34fc99ccc36f2e51ed7965dc5daae0ff271
erickTornero <erickdeivy01@gmail.com>
2019-12-01 05:04:03 -0500

    [add] plot std-trajectory

--- utils/analize_rewards.py
+++ utils/analize_rewards.py
@@ -5,7 +5,8 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from IPython.core.debugger import set_trace
-def plot_rewards(list_paths, smooth_factor=0.5):
+from utils.plotter import drawStdPlot
+def plot_rewards(list_paths, smooth_factor=0.5, history=None):
     assert smooth_factor < 0.99, 'Smooth factor must be between 0.0 & 0.99'
     dirs    =   []
     for path in list_paths:
@@ -16,7 +17,7 @@ def plot_rewards(list_paths, smooth_factor=0.5):
 
     n_samples   =   [len(s_rws) for s_rws in sample_rws]
     #sample_rws  =   [sorted(sample) for sample in sample_rws]
-
+    set_trace()
     rws_paths   =   []
     for nsample, dirname in zip(n_samples, dirs):
         rewards =   []
@@ -39,10 +40,47 @@ def plot_rewards(list_paths, smooth_factor=0.5):
 
     for rws in rws_paths:
         plt.plot(rws)
+    
+    if history is not None: plt.legend(history)
 
     plt.grid(True)
     plt.show()
 
-l = ['data/sample25','data/sample26', 'data/sample27']
 
-plot_rewards(l, 0.8)
+
+def get_rewards_lists(list_paths, smooth_factor=0.5, history=None):
+    assert smooth_factor < 0.99, 'Smooth factor must be between 0.0 & 0.99'
+    dirs    =   []
+    for path in list_paths:
+        if os.path.exists(path): dirs.append(path)
+    
+    #set_trace()
+    sample_rws  =   [glob.glob(os.path.join(path,'rewards/rewards_it_*.pkl')) for path in dirs]
+
+    n_samples   =   [len(s_rws) for s_rws in sample_rws]
+    max_length  =   min(n_samples)
+    n_samples   =   [max_length for _ in n_samples]
+    #sample_rws  =   [sorted(sample) for sample in sample_rws]
+    set_trace()
+    rws_paths   =   []
+    for nsample, dirname in zip(n_samples, dirs):
+        rewards =   []
+        for fn_it in range(1, nsample+1):
+            rw_it   =   joblib.load(os.path.join(dirname, 'rewards/rewards_it_'+str(fn_it)+'.pkl'))
+            rewards.append(np.mean(rw_it))
+        rws_paths.append(rewards)
+    
+    return rws_paths
+
+def plotStdPlotComparison(list_paths, smooth_factor=0.5, history=None):
+    rewards_list    =   get_rewards_lists(list_paths, smooth_factor, history)
+    axes            =   drawStdPlot(rewards_list, 'title', 'x','y')
+    plt.show()
+
+#l = ['data/sample16','data/sample25','data/sample26', 'data/sample27', 'data/sample28', 'data/sample29']
+
+#l = ['data/sample4','data/sample5','data/sample6', 'data/sample7', 'data/sample12', 'data/sample13']
+#l = ['data/sample5','data/sample6', 'data/sample7']
+l = ['data/sample40', 'data/sample47']
+#plot_rewards(l, 0.75 , l)
+plotStdPlotComparison(l)
---
+++ utils/plotter.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def drawStdPlot( batchResults, title, xlabel, ylabel, startStep = 1, color = 'r', axes = None, scalefactor=1 ) : 
+    """
+        This Function was substracted from W. Pumacay, 
+        https://github.com/wpumacay/DeeprlND-projects/
+        Draws an std-plot from a batch of results from various experiments
+    
+    Args:
+        batchResults (list): list of experiments, each element containing results
+                             for each expriment (training run) as a list of elements
+    """
+    # sanity-check: should have at least pass some data
+    assert len( batchResults ) > 0, 'ERROR> should have passed at least some data'
+
+    # convert batch to user friendly np.ndarray
+    batchResults = np.array( batchResults )
+
+    # if no axes given, create a new one
+    if axes is None :
+        fig, axes = plt.subplots()
+
+    # each element has _niters elements on it (iters: episodes, steps, etc.)
+    _niters = batchResults.shape[1]
+    _xx = (np.arange( _niters ) + startStep) * scalefactor
+
+    # grab mean and std over all runs
+    _mean = batchResults.mean( axis = 0 )
+    _std = batchResults.std( axis = 0 )
+
+    # do the plotting
+    axes.plot( _xx, _mean, color = color, linestyle = '-' )
+    axes.fill_between( _xx, _mean - 2. * _std, _mean + 2. * _std, color = color, alpha = 0.5 )
+
+    axes.set_xlabel( xlabel )
+    axes.set_ylabel( ylabel )
+    axes.set_title( title )
+
+    return axes
